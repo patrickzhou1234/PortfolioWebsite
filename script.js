57986266856276d836f821850d8569c2a534d02b
Patrick Zhou <patrickchang44543@gmail.com>
2023-08-20 11:13:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ musicnote = document.getElementById("musicnote");
 fetch("https://api.ipify.org?format=json")
   .then((t) => t.json())
   .then((t) => {
-    fetch("https://damn.gq/api/", {
+    fetch("https://damn.gq/api/isadev/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ ip: t.ip }),
